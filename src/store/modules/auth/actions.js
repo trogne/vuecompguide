6d417aa6a22a54b5d3c18a50c9c86f377caa537d
Patrick Fiset <patrick.fiset@gmail.com>
2021-03-24 16:45:13 -0400

    firekey

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -16,11 +16,11 @@ export default {
   async auth(context, payload) {
     const mode = payload.mode;
     let url =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAE9itMMz96MUmnH7I3kLL7FT2JHMW5m7s';
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_FIREBASE_KEY}`;
 
     if (mode === 'signup') {
       url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAE9itMMz96MUmnH7I3kLL7FT2JHMW5m7s';
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_FIREBASE_KEY}`;
     }
     const response = await fetch(url, {
       method: 'POST',
